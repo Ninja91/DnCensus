0cedf8412e19fc62f98f1a5dd3422385d5265a75
Nitin Jain <jain117@purdue.edu>
2015-11-04 00:14:52 -0500

    Building index map using marisa_trie

--- Scripts/preprocessing.py
+++ Scripts/preprocessing.py
@@ -1,24 +1,42 @@
 #!/usr/bin/python
 
 __author__ = 'Nitin'
+
+
+import marisa_trie as mt
 from csv import DictReader
-import cPickle as Pickle
+# import cPickle as Pickle
 
-BP = dict()
-with open('aaaa.csv', 'rb') as f:
-    reader = DictReader(f)
-    print reader.fieldnames
 
-    for row in reader:
+def index_ip6address_4_aaaa():
+    ip6_list = []
+    ip6_trie_list = []
 
-    # for row in reader:
-    #     if BP.has_key(row['name']):
-    #         if row['ip4address'] in BP[row['name']]:
-    #             continue
-    #         else:
-    #             BP[row['name']].append(row['ip4address'])
-    #     else:
-    #         BP[row['name']] = [row['ip4address']]
-    #
-    # f = open('map.pkl', 'wb')
-    # Pickle.dump(BP, f)
+    with open('../records/aaaa.csv', 'r') as f:
+        reader = DictReader(f)
+        print reader.fieldnames
+
+        for row in reader:
+            ip6_list.append(row[reader.fieldnames[2]])
+            if len(ip6_list) > 14000000:
+                ip6_trie_list.append(mt.Trie(ip6_list))
+                ip6_list = []
+                print "Success"
+                break
+                
+
+        # data = open('index1.pkl', 'wb')
+        # Pickle.dump(ip6_address_list, data)
+
+        # for row in reader:
+        #     if BP.has_key(row['name']):
+        #         if row['ip4address'] in BP[row['name']]:
+        #             continue
+        #         else:
+        #             BP[row['name']].append(row['ip4address'])
+        #     else:
+        #         BP[row['name']] = [row['ip4address']]
+        #
+        # f = open('map.pkl', 'wb')
+        # Pickle.dump(BP, f)
+index_ip6address_4_aaaa()
