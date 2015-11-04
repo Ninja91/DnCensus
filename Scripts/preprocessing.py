eb7b44794ce5770f1177d33fb55ddadc6e8e5808
Nitin Jain <jain117@purdue.edu>
2015-11-04 00:18:14 -0500

    Updating scripts

--- Scripts/preprocessing.py
+++ Scripts/preprocessing.py
@@ -5,10 +5,17 @@ __author__ = 'Nitin'
 
 import marisa_trie as mt
 from csv import DictReader
-# import cPickle as Pickle
+import os.path
+import cPickle as Pickle
 
 
 def index_ip6address_4_aaaa():
+    if os.path.isfile('index.pkl'):
+        print 'Loaded'
+        trie = Pickle.load('index.pkl')
+        print trie['::ffff:74.117.221.143']
+        return [trie]
+
     ip6_list = []
     ip6_trie_list = []
 
@@ -23,20 +30,51 @@ def index_ip6address_4_aaaa():
                 ip6_list = []
                 print "Success"
                 break
-                
+        ip6_trie_list.append(mt.Trie(ip6_list))
+        ip6_trie_list[0].save('index.marisa')
+        Pickle.dump(ip6_trie_list[0], 'index.pkl')
 
-        # data = open('index1.pkl', 'wb')
-        # Pickle.dump(ip6_address_list, data)
+        print "Success"
+        return ip6_trie_list
 
-        # for row in reader:
-        #     if BP.has_key(row['name']):
-        #         if row['ip4address'] in BP[row['name']]:
-        #             continue
-        #         else:
-        #             BP[row['name']].append(row['ip4address'])
-        #     else:
-        #         BP[row['name']] = [row['ip4address']]
-        #
-        # f = open('map.pkl', 'wb')
-        # Pickle.dump(BP, f)
-index_ip6address_4_aaaa()
+
+def build_map(ip6_trie_list, filename='../records/aaaa.csv'):
+    with open(filename, 'r') as f:
+        reader = DictReader(f)
+        current = None
+        current_host = None
+        count = 0
+        i = 0
+
+        keys = []
+        values = []
+        if len(ip6_trie_list) == 1:
+            ip6_dict = ip6_trie_list[0]
+
+        for row in reader:
+            i += 1
+            if i >= 5:
+                print keys, values
+                break
+            prev = current
+            current = row[reader.fieldnames[2]]
+            print prev, current
+            prev_host = current_host
+            current_host = row[reader.fieldnames[0]]
+            print prev_host, current_host
+            if prev_host != current_host:
+                keys.append(prev_host)
+                values.append((ip6_dict.key_id(prev), count))
+                count = 1
+            else:
+                if prev != current:
+                    keys.append(prev_host)
+                    values.append((ip6_dict.key_id(prev), count))
+                    count = 1
+                else:
+                    count += 1
+
+
+dictionary = index_ip6address_4_aaaa()
+print dictionary['::ffff:74.117.221.143']
+# build_map(dictionary)
